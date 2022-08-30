63578cb2e571b2a4585ff7ca04138fa81c1c8e47
Reggie899 <regina.schmidt1212@gmail.com>
2022-08-30 16:41:11 +0200

    container for inbox added

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,18 +1,31 @@
 import React from "react";
+import Mails from "./Mails";
 
-
-function HomeComponent() {
-return (
-
-<div className="home">
-<container>
-    
-</container>
+const navigationBarItems = <div className="menu">
+    <div>All</div>
+    <div>Read</div>
+    <div>Unread</div>
+    <div>Spam</div>
+    <div>Deleted</div>
 
 </div>
 
-)
-
+function HomeComponent() {
+  return (
+    <div className="home">
+      <container>
+        <div className="sidebar">
+            <Mails />
+        </div>
+        <div className="rightside">
+          <div className="navigationBar">
+              {navigationBarItems}
+              </div> 
+          <div className="mailview"></div>
+        </div>
+      </container>
+    </div>
+  );
 }
 
-export default HomeComponent
+export default HomeComponent;
---
+++ src/components/Mails.js
@@ -0,0 +1,21 @@
+
+
+// Call from Mongo DB 20 Mails 
+// + 5 Deleted
+
+
+
+
+
+
+function Mails() {
+    return (
+      <div className="mail">
+       <li>
+Item 1 
+       </li>
+      </div>
+    );
+  }
+  
+  export default Mails;
